484dc4e8ea965fa50fe89c1c29568ec3f45f3dfb
ikopke23 <ian.kopke23@trinityschoolnyc.org>
2022-11-22 08:53:51 -0800

    MS3 done

--- prebuild.js
+++ prebuild.js
@@ -3,3 +3,13 @@
   fs.rmSync('public', { recursive: true, force: true });
   fs.mkdirSync('public');
   fs.copyFileSync('./src/styles.css', './public/styles.css');
+  fs.copyFileSync('./src/views/about.html', './public/about.html');
+  // fs.renameSync('./src/views/wireframe', './public/wireframe');
+  fs.mkdirSync('public/wireframe');
+
+  let pics = fs.readdirSync('./src/views/wireframe');
+  pics.forEach(function(pic){
+    fs.copyFileSync(`./src/views/wireframe/${pic}`, './public/wireframe/'+pic)
+  });
+
+    // console.log(fs.readdirSync('./src/views/wireframe'));
