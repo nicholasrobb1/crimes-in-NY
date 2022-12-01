cb05e4f8b473222098605f52544d2d1e5843aeaf
ikopke23 <ian.kopke23@trinityschoolnyc.org>
2022-12-01 11:07:42 -0800

    h

--- prebuild.js
+++ prebuild.js
@@ -3,7 +3,6 @@
   fs.rmSync('public', { recursive: true, force: true });
   fs.mkdirSync('public');
   fs.copyFileSync('./src/styles.css', './public/styles.css');
-  fs.copyFileSync('./src/views/about.html', './public/about.html');
   // fs.renameSync('./src/views/wireframe', './public/wireframe');
   fs.mkdirSync('public/wireframe');
 
